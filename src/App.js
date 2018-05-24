2c3b5468cbdad9e2bf594a9ea4c55b8ac1d952c8
Levi Durfee <levi.durfee@gmail.com>
2018-05-24 11:58:45 -0400

    login

--- src/App.js
+++ src/App.js
@@ -1,27 +1,52 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      authenticated: false
+      authenticated: false,
+      email: '',
+      password: ''
     }
+    this.handleChange = this.handleChange.bind(this);
   }
   render() {
-    if(this.state.authenticated == false) {
+    if(this.state.authenticated === false) {
       return (
         <div>
-          Gotta login
+          <input type="text" id="email" placeholder="email" value={this.state.email} onChange={this.handleChange} />
+          <input type="text" id="password" placeholder="password" value={this.state.password} onChange={this.handleChange} />
+          <button onClick={this.login}>Login</button>
         </div>
       )
     }
-    
+
     return (
       <div className="App">
-
+        Logged in
       </div>
     );
+  }
+
+  login = event => {
+    event.preventDefault();
+    axios.post('http://localhost:8000/v1/auth', {
+      email: this.state.email,
+      password: this.state.password
+    })
+    .then((response) => {
+      if(response.data.status === "success") {
+        this.setState({authenticated: true});
+      }
+    })
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
   }
 }
 
