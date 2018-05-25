a7f725998fe0fb12fdb8c1dfd1fa5fac0f6b213e
Levi Durfee <levi.durfee@gmail.com>
2018-05-25 11:14:32 -0400

    link to disclaimer

---
+++ src/components/Disclaimer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Disclaimer.css';
+
+export default class Disclaimer extends Component {
+  render() {
+	  return (
+	    <div className="Disclaimer">
+				<p><a href="disclaimer.html" target="_blank">Disclaimer</a></p>
+	    </div>
+	  )
+  }
+}
