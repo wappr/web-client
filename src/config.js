9d78033fd82119ddde41b83576854a79964463ee
Levi Durfee <levi.durfee@gmail.com>
2018-05-27 12:15:58 -0400

    buddy request build

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-export const API_SERVER = 'http://localhost:8000/';
+//export const API_SERVER = 'http://localhost:8000/';
+export const API_SERVER = 'https://api.wappr.net/';
 export const API_VERSION = 'v1';
