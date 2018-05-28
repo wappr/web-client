62bf336f39e8a05299fc8183d99da2a8256b2415
Levi Durfee <levi.durfee@gmail.com>
2018-05-28 09:40:56 -0400

    add sw back

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-//import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-//registerServiceWorker();
-console.log('Client Version: ', '0.1.0');
+registerServiceWorker();
+console.log('Client Version: ', '0.2.0');
